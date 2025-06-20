8c5c0e5f18d6769be018d295e38f2170bf7ff5cd
iamgp21 <gaupande21@gmail.com>
2025-06-20 11:47:13 +0530

    updated module source path

--- argocd/wsl2/main.tf
+++ argocd/wsl2/main.tf
@@ -1,5 +1,6 @@
 module "argocd" {
-  source = "./modules/argocd"
+  # source = "../../modules/argocd"
+  source = "tf-helm-deployer/modules/argocd"
   argo_cd = var.argo_cd
   argocd_admin_password = var.argocd_admin_password
   values = var.values
