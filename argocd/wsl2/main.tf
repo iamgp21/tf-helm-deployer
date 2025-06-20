6cb80f015e0f6815494993800ef579876289ca88
iamgp21 <gaupande21@gmail.com>
2025-06-20 11:56:25 +0530

    updated code

--- argocd/wsl2/main.tf
+++ argocd/wsl2/main.tf
@@ -1,6 +1,6 @@
 module "argocd" {
   # source = "../../modules/argocd"
-  source = "../../modules/argocd"
+  source = "github.com/iamgp21/tf-helm-deployer//modules/argocd"
   argo_cd = var.argo_cd
   argocd_admin_password = var.argocd_admin_password
   values = var.values
